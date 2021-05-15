30a79c77e4334762e089758e1118944c43c04164
vjjsn <83721603+vjjsn@users.noreply.github.com>
2021-05-15 12:26:49 +0700

    the tab button is done

--- tab-button.js
+++ tab-button.js
@@ -3,13 +3,24 @@ const buttons = document.querySelectorAll('.todo-tab-card')
 buttons[0].classList.add('is-active')
 
 buttons.forEach(button => {
+    let x = button.offsetLeft
     button.addEventListener('click', e => {
         buttons.forEach(button => {
-            button.classList.remove('is-active')
+           button.classList.remove('is-active')
         })
         e.currentTarget.classList.add('is-active')
-        console.log("clicked")
+        document.getElementById('todo-tab-button').style.left = x + "px"
     })
 })
+
+//
+
+
+
+
+
+
+
+
 
 
